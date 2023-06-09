f16687acc924d292511c2dd70ad4165d02b06322
Zoe Gao <122059026+zoelmg@users.noreply.github.com>
2023-06-09 19:21:59 -0400

    Added missing javadoc

--- src/main/java/cs3500/pa04/json/GameType.java
+++ src/main/java/cs3500/pa04/json/GameType.java
@@ -4,6 +4,12 @@ package cs3500.pa04.json;
  * The type of game the Server should host
  */
 public enum GameType {
+  /**
+   * A game played in Single mode
+   */
   SINGLE,
+  /**
+   * A game played in Multi Mode
+   */
   MULTI
 }
--- src/main/java/cs3500/pa04/json/ShipDirection.java
+++ src/main/java/cs3500/pa04/json/ShipDirection.java
@@ -4,6 +4,12 @@ package cs3500.pa04.json;
  * The direction of the ship on the board
  */
 public enum ShipDirection {
+  /**
+   * the vertical direction of the ship
+   */
   VERTICAL,
+  /**
+   * the horizontal direction of the ship
+   */
   HORIZONTAL
 }
